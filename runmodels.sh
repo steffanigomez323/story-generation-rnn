7e1379910d989bc1b402f907c0886d60798d31c6
Steffani Gomez <steffani_gomez@brown.edu>
2016-12-04 09:34:15 -0500

    ran more initial results on word generation

--- runmodels.sh
+++ runmodels.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 source /course/cs2950k/tf_gpu_venv/bin/activate
 
-datasets=( "edgarallenpoe" "finneganswake" "nightvale" "quentintarantino" "smallshakespeare" )
+datasets=( "finneganswake" "nightvale" "quentintarantino" "smallshakespeare" )
 
 for d in "${datasets[@]}"
 do
     python run.py --label $d --input_text data/$d.txt
 done
 
-#python run.py --label asongoffireandice --input_text data/asongoffireandice.txt --batch_size 25 --step_size 25 
+python run.py --label asongoffireandice --input_text data/asongoffireandice.txt --batch_size 25 --step_size 25 
