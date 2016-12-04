82f3c13e5a6852a6f55f228e2798b8de9cc9b0a2
S2 <S2@brown.edu>
2016-12-04 00:28:28 -0500

    added initial word generation models

--- runmodels.sh
+++ runmodels.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 source /course/cs2950k/tf_gpu_venv/bin/activate
 
-python run.py --label asongoffireandice --input_text data/asongoffireandice.txt
-
 #datasets=( "finneganswake" "nightvale" "quentintarantino" "asongoffireandice" )
 
-#for d in "${datasets[@]}"
-#do
-#    python run.py --label $d --input_text data/$d.txt
-#done
+datasets=( "lolita" "xfilesseason1" )
+
+for d in "${datasets[@]}"
+do
+    python run.py --label $d --input_text data/$d.txt
+done
