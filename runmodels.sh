1106ed1d256648bea13352cf927ea07b7fa7b11a
Steffani Gomez <steffani_gomez@brown.edu>
2016-12-04 20:26:55 -0500

    updating bash script

--- runmodels.sh
+++ runmodels.sh
@@ -1,12 +1,20 @@
 #!/bin/bash 
 source /course/cs2950k/tf_gpu_venv/bin/activate
 
-datasets=( "xfilesalloriginal" )
+datasets=( "xfilesalloriginal" "edgarallenpoe" "finneganswake" "lolita" "nightvale" "quentintarantino" "smallshakespeare" "asongoffireandice")
+
+lstm=256
+layer=3
+batchsize=25
+stepsize=25
 
 for d in "${datasets[@]}"
 do
-    python run.py --label $d --input_text data/$d.txt --words False
-    python run.py --label $d --input_text data/$d.txt --batch_size 25 --step_size 25
+    python run.py --label $d --input_text data/$d.txt --words False --lstm_size $lstm
+    python run.py --label $d --input_text data/$d.txt --words False --num_layers $layer
+    python run.py --label $d --input_text data/$d.txt --words False --batch_size $batchsize
+    python run.py --label $d --input_text data/$d.txt --words False --step_size $stepsize
+#    python run.py --label $d --input_text data/$d.txt --batch_size 25 --step_size 25
 done
 
 #python run.py --label asongoffireandice --input_text data/asongoffireandice.txt --batch_size 25 --step_size 25
