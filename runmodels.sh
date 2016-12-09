544a04635cb33094a53f3277d2b20f383cce8902
Steffani Gomez <steffani_gomez@brown.edu>
2016-12-08 23:27:24 -0500

    fixing bash script

--- runmodels.sh
+++ runmodels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source venv/bin/activate
-source /course/cs2950k/tf_gpu_venv/bin/activate
+#source venv/bin/activate
+#source /course/cs2950k/tf_gpu_venv/bin/activate
 
 datasets=( "edgarallenpoe" "finneganswake" "lolita" "nightvale" "quentintarantino" "smallshakespeare" )
 
@@ -18,10 +18,10 @@ do
     python run.py $d lda data/$d.txt $out -num_topics $numtopics
     python run.py $d lda data/$d.txt $out -doclen $doclen
 
-    python run.py $d rnn data/$d.txt $out -lstm_size $lstm
-    python run.py $d rnn data/$d.txt $out -num_layers $layer
-    python run.py $d rnn data/$d.txt $out -batch_size $batchsize
-    python run.py $d rnn data/$d.txt $out -step_size $stepsize
+#    python run.py $d rnn data/$d.txt $out -lstm_size $lstm
+#    python run.py $d rnn data/$d.txt $out -num_layers $layer
+#    python run.py $d rnn data/$d.txt $out -batch_size $batchsize
+#    python run.py $d rnn data/$d.txt $out -step_size $stepsize
 done
 
 #python run.py --label asongoffireandice --input_text data/asongoffireandice.txt --batch_size 25 --step_size 25
